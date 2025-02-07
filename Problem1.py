3606e43612f54081562dad2e5673e8144553ec3b
manzun9685 <manzun9685@stu.mylusd.org>
2025-02-07 08:32:54 -0800

    Update Problem1.py

--- Problem1.py
+++ Problem1.py
@@ -1,4 +1,55 @@
+#region VEXcode Generated Robot Configuration
+from vex import *
+import urandom
 
+# Brain should be defined by default
+brain=Brain()
+
+# Robot configuration code
+motor_2 = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+distance_1 = Distance(Ports.PORT1)
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = brain.battery.voltage(MV) + brain.battery.current(CurrentUnits.AMP) * 100 + brain.timer.system_high_res()
+    urandom.seed(int(random))
+      
+# Set random seed 
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+#endregion VEXcode Generated Robot Configuration
+
+# ------------------------------------------
+# 
+# 	Project:      VEXcode Project
+#	Author:       VEX
+#	Created:
+#	Description:  VEXcode V5 Python Project
+# 
+# ------------------------------------------
+
+# Library imports
+from vex import *
+
+# Begin project code
 while  1==1:   
     if distance_1.is_object_detected():
         pass
